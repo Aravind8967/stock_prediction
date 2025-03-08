0303e288a5cddc8b43323602775816da783dd4fc
Aravind <aravindvchindi@gmail.com>
2025-03-08 22:11:32 +0530

    trial

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ def share_price_arr(c_name):
     price_arr = analys.share_price_range('1mo', '1d')
     return jsonify(price_arr)
 
-def add(a, b):
-    return a+b
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
